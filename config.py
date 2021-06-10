6516ef36a803b1554d2bd50eff840387aada8102
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2021-06-10 22:15:06 +0200

    added enpoints for getting all and specified logs data

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+
+
 class Config:
     APP_PORT = 8080
     APP_HOST = "0.0.0.0"
+    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
     #Logging hours
     LOGGING_SCHEDULE = [x for x in range(24)]
--- routes/main.py
+++ routes/main.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, jsonify
+from flask import current_app as app
 from station import I2C_Station
 from response_data import ResponseData
 import os
+import json
 
 
 main_ = Blueprint("main", __name__, template_folder='template', static_folder='static')
+CURRENT_DIR = app.config["CURRENT_DIR"]
+LOGS_PATH = app.config["LOGS_PATH"]
 
 WeatherStation = I2C_Station()
 
@@ -34,3 +38,26 @@ def weather():
     response_data = ResponseData(WeatherStation.get_weather_sensor_keys(), values)
 
     return response_data.create_response()
+
+
+@main_.route("/api/get_logged_days")
+def get_logged_days():
+    logs_path = os.path.join(CURRENT_DIR, LOGS_PATH)
+    logs = [log for log in os.listdir(logs_path)]
+
+    return jsonify(logs)
+
+
+@main_.route("/api/get_weather_data/<date>")
+def get_weather_data(date):
+    log_name = f"{date}.json"
+    log_path = os.path.join(CURRENT_DIR, os.path.join(LOGS_PATH, log_name))
+
+    if os.path.exists(log_path):
+        with open(log_path, "r") as log_file:
+            log_data = json.loads(log_file.read())
+    else:
+        log_data = {}
+        log_data["error"] = "Log file not found"
+
+    return jsonify(log_data)
